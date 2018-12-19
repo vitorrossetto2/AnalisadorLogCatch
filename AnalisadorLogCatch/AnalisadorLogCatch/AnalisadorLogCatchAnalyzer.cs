48016e7ecf321c48426ca495922c057f53539344
Verity - Vitor Rossetto <verity.vitor@unidas.com.br>
2018-12-19 00:26:26 -0200

    Implementação code fix provider

--- AnalisadorLogCatch/AnalisadorLogCatch/AnalisadorLogCatchAnalyzer.cs
+++ AnalisadorLogCatch/AnalisadorLogCatch/AnalisadorLogCatchAnalyzer.cs
@@ -15,36 +15,105 @@ namespace AnalisadorLogCatch
     {
         public const string DiagnosticId = "AnalisadorLogCatch";
 
-        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Localizing%20Analyzers.md for more on localization
         private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
         private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
-        private const string Category = "Naming";
+        private const string Category = "Boas práticas";
 
-        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+        private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
 
         public override void Initialize(AnalysisContext context)
         {
-            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
-            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.NamedType);
+            //registrando para analisar TODOS os blocos de código
+            context.RegisterCodeBlockAction(AnalyseCodeBlockAction);
         }
 
-        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        private static void AnalyseCodeBlockAction(CodeBlockAnalysisContext context)
         {
-            // TODO: Replace the following code with your own analysis, generating Diagnostic objects for any issues you find
-            var namedTypeSymbol = (INamedTypeSymbol)context.Symbol;
 
-            // Find just those named type symbols with names containing lowercase letters.
-            if (namedTypeSymbol.Name.ToCharArray().Any(char.IsLower))
+            //Procurando diretivas try no código
+            foreach (var @try in context.CodeBlock.DescendantNodes().OfType<TryStatementSyntax>())
             {
-                // For all such symbols, produce a diagnostic.
-                var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], namedTypeSymbol.Name);
+                //Procurando todos os catchs do try
+                foreach (var @catch in @try.Catches)
+                {
+                    var declaracaoCatch = @catch.DescendantNodes().OfType<CatchDeclarationSyntax>().FirstOrDefault();
 
-                context.ReportDiagnostic(diagnostic);
+                    string nomeVariavelException = null;
+
+                    //se não há nada no catch ja reporta erro no código
+                    if (declaracaoCatch == null)
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, @catch.GetLocation());
+                        context.ReportDiagnostic(diagnostic);
+                        continue;
+                    }
+                    else
+                    {
+                        //procurando por variáveis no catch (se estiver somente com o catch sem a váriavel já reporta erro)
+                        // Ex: catch(Exception){     =====> ERRADO
+                        var declaracaoVariavel = declaracaoCatch.ChildTokens().FirstOrDefault(x => x.Kind() == SyntaxKind.IdentifierToken);
+                        if (declaracaoVariavel == null || string.IsNullOrEmpty(declaracaoVariavel.Text))
+                        {
+                            var diagnostic = Diagnostic.Create(Rule, @catch.GetLocation());
+                            context.ReportDiagnostic(diagnostic);
+
+                            continue;
+                        }
+                        else
+                        {
+                            //separo o nome da váriavel para posteriormente verificar se ela foi utilizada no log
+                            nomeVariavelException = declaracaoVariavel.Text;
+                        }
+                    }
+
+                    var chamadasMetodos = @catch.DescendantNodes().OfType<ExpressionStatementSyntax>();
+
+                    bool encontrouTratamentoEsperado = false;
+
+                    //procurando todas as chamadas de métodos dentro do catch
+                    foreach (var chamadaMetodo in chamadasMetodos)
+                    {
+                        var acessoMembro = chamadaMetodo.Expression as InvocationExpressionSyntax;
+
+                        if(acessoMembro != null)
+                        {
+                            //encontrado uma chamada para algum método
+                            var identificadores = acessoMembro.DescendantNodes().OfType<IdentifierNameSyntax>();
+
+                            if(identificadores.Count() > 1)
+                            {
+                                //Obtendo o nome do método chamado (dos identificadores é o que fica na posição abaixo)
+                                var identificadorChamadaMetodo = identificadores.ElementAt(1);
+
+                                if (identificadorChamadaMetodo.Identifier.ToFullString().Contains("Error") ||
+                                    identificadorChamadaMetodo.Identifier.ToFullString().Contains("Warn") ||
+                                    identificadorChamadaMetodo.Identifier.ToFullString().Contains("Fatal"))
+                                {
+
+                                    //procurando pelos parâmetros passados no métodos (um deles TEM que ser a variavel do catch)
+                                    var parametrosMetodo = acessoMembro.DescendantNodes().OfType<ArgumentSyntax>()
+                                        .SelectMany(x => x.DescendantTokens()).Where(x => x.Kind() == SyntaxKind.IdentifierToken);
+
+                                    if (parametrosMetodo.Any(x => x.Text == nomeVariavelException))
+                                    {
+                                        encontrouTratamentoEsperado = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    //se não encontrou o tratamento reporta o erro no catch
+                    if (!encontrouTratamentoEsperado)
+                    {
+                        var diagnostic = Diagnostic.Create(Rule, @catch.GetLocation());
+
+                        context.ReportDiagnostic(diagnostic);
+                    }
+                }
             }
         }
     }
--- AnalisadorLogCatch/AnalisadorLogCatch/AnalisadorLogCatchCodeFixProvider.cs
+++ AnalisadorLogCatch/AnalisadorLogCatch/AnalisadorLogCatchCodeFixProvider.cs
@@ -12,13 +12,14 @@ using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Rename;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis.Formatting;
 
 namespace AnalisadorLogCatch
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AnalisadorLogCatchCodeFixProvider)), Shared]
     public class AnalisadorLogCatchCodeFixProvider : CodeFixProvider
     {
-        private const string title = "Make uppercase";
+        private const string title = "Incluir Logs";
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds
         {
@@ -27,7 +28,6 @@ namespace AnalisadorLogCatch
 
         public sealed override FixAllProvider GetFixAllProvider()
         {
-            // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
             return WellKnownFixAllProviders.BatchFixer;
         }
 
@@ -35,39 +35,64 @@ namespace AnalisadorLogCatch
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
 
-            // TODO: Replace the following code with your own analysis, generating a CodeAction for each fix to suggest
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the type declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<TypeDeclarationSyntax>().First();
+            //encontrando o catch que estava com erro
+            var @catch = (CatchClauseSyntax)root.FindNode(diagnosticSpan);
 
-            // Register a code action that will invoke the fix.
             context.RegisterCodeFix(
                 CodeAction.Create(
                     title: title,
-                    createChangedSolution: c => MakeUppercaseAsync(context.Document, declaration, c),
+                    createChangedDocument: c => FixWrongCode(context.Document, @catch, c),
                     equivalenceKey: title),
                 diagnostic);
         }
 
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
+        private async Task<Document> FixWrongCode(Document document, CatchClauseSyntax @catch, CancellationToken cancellationToken)
         {
-            // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
+            //procurando o bloco do catch
+            var blocoCatch = @catch.DescendantNodes().OfType<BlockSyntax>().First();
 
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
+            //criando a seguinte expressão : logger.Error(ex, "Erro na classe")
+            var chamadaLog = SyntaxFactory.ExpressionStatement(
+                                SyntaxFactory.InvocationExpression(
+                                    SyntaxFactory.MemberAccessExpression(
+                                        SyntaxKind.SimpleMemberAccessExpression,
+                                        SyntaxFactory.IdentifierName("logger"),
+                                        SyntaxFactory.IdentifierName("Error")))
+                            .WithArgumentList(
+                                SyntaxFactory.ArgumentList(
+                                    SyntaxFactory.SeparatedList<ArgumentSyntax>(
+                                        new SyntaxNodeOrToken[]{
+                                            SyntaxFactory.Argument(
+                                                SyntaxFactory.IdentifierName("ex")),
+                                            SyntaxFactory.Token(
+                                                SyntaxFactory.TriviaList(),
+                                                SyntaxKind.CommaToken,
+                                                SyntaxFactory.TriviaList(
+                                                    SyntaxFactory.Space)),
+                                            SyntaxFactory.Argument(
+                                                SyntaxFactory.LiteralExpression(
+                                                    SyntaxKind.StringLiteralExpression,
+                                                    SyntaxFactory.Literal("Erro na classe")))}))))
+                            .WithSemicolonToken(
+                                SyntaxFactory.Token(
+                                    SyntaxFactory.TriviaList(),
+                                    SyntaxKind.SemicolonToken,
+                                    SyntaxFactory.TriviaList(
+                                        SyntaxFactory.LineFeed)));
 
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
+            //inserindo o novo código antes do primeiro nó do catch
+            var novoBlocoCatch = blocoCatch.InsertNodesBefore(blocoCatch.ChildNodes().First(), new SyntaxList<StatementSyntax>(new StatementSyntax[] { chamadaLog }));
 
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
+            //formatando (como se fosse CTRL + K + D)
+            var novoBlocoCatchFormatado = novoBlocoCatch.WithAdditionalAnnotations(Formatter.Annotation);
+
+            var oldRoot = await document.GetSyntaxRootAsync(cancellationToken);
+            var newRoot = oldRoot.ReplaceNode(blocoCatch, novoBlocoCatchFormatado);
+
+            return document.WithSyntaxRoot(newRoot);
         }
     }
 }
